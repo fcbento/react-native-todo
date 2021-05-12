2c771775f2eb9e6eafa49a4757d39b1848a2ca7f
Felie Bento <felipe.16costa@gmail.com>
2021-05-12 13:22:30 -0300

    fixes

--- src/screens/shopping/shopping.js
+++ src/screens/shopping/shopping.js
@@ -7,61 +7,80 @@ import { set } from 'react-native-reanimated';
 export default function Shopping(props) {
 
     const [products, setProducts] = useState([]);
-
-    const addOrRemove = (product, type) => {
-
-        products.forEach((item) => {
-            if (product.productId == item.productId) {
-
-                if (type === 'add') {
-                    product.cartQuantity < product.quantity ? item.cartQuantity++ : 0;
-                }
-
-                if (type === 'remove') {
-                    product.cartQuantity > 1 ? item.cartQuantity-- : 0;
-                }
-            }
-        });
-
-        setProducts([...products]);
-    }
+    const [totalPrice, setTotalPrice] = useState([]);
 
     useEffect(() => {
 
         props.cart.forEach(element => {
             element.cartQuantity = 1;
         });
+        
+        setStates(props.cart);
 
-        setProducts([...props.cart]);
+    }, []);
 
-    }, [])
+    const addOrRemove = (product, type) => {
+
+        products.forEach((item) => {
+            if (product.productId == item.productId) {
+                if (type === 'add')
+                    product.cartQuantity < product.quantity ? item.cartQuantity++ : 0;
+                else
+                    product.cartQuantity > 1 ? item.cartQuantity-- : 0;
+            }
+        });
+
+        setStates(products);
+    }
+
+    const setStates = (arr) => {
+        setTotalPrice(formatMoney(sumItems(arr)));
+        setProducts([...arr]);
+    }
+
+    const sumItems = (arr) => {
+        return arr.reduce((acc, product) => {
+            return acc + (product.cartQuantity * product.price)
+        }, 0);
+    }
+
+    const formatMoney = (value) => {
+        return value ? value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') : 0;
+    }
 
     return (
+        <>
+            <Card>
+                <Title >TOTAL : $ {totalPrice}</Title>
+            </Card>
 
-        <Container>
+            <Container>
 
-            {products.map((product, index) => (
-                <Card key={index}>
+                {products.map((product, index) => (
 
-                    <Description> {product.category.name} </Description>
-                    <Title >{product.name}</Title>
+                    <Card key={index}>
 
-                    <Image
-                        style={{ width: '100%', height: 240 }}
-                        source={{
-                            uri: `${product.image}`,
-                        }}
-                    />
+                        <Description> {product.category.name} </Description>
+                        <Title >{product.name}</Title>
 
-                    <Text>
-                        <AntDesign name="minuscircle" size={24} color="black" onPress={() => addOrRemove(product, 'remove')} />
-                        <AntDesign name="pluscircle" size={24} color="black" onPress={() => addOrRemove(product, 'add')} />
-                    </Text>
+                        <Image
+                            style={{ width: '100%', height: 240 }}
+                            source={{
+                                uri: `${product.image}`,
+                            }}
+                        />
 
-                    <Description> On cart: {product.cartQuantity} </Description>
-                    <Price>$ {product.price * product.cartQuantity}</Price>
-                </Card>
-            ))}
-        </Container>
+                        <Text>
+                            <AntDesign name="minuscircle" size={24} color="black" onPress={() => addOrRemove(product, 'remove')} />
+                            <AntDesign name="pluscircle" size={24} color="black" onPress={() => addOrRemove(product, 'add')} />
+                        </Text>
+
+                        <Description> On cart: {product.cartQuantity} </Description>
+                        <Price>$ {formatMoney(product.price * product.cartQuantity)}</Price>
+                    </Card>
+                ))}
+
+            </Container>
+        </>
     )
 }
